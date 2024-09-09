3f5690eccb5f9238054b54a833bbeb9d103af9be
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2024-09-09 22:16:36 +0500

    Add entity Class and add dto folders

---
+++ HomeUI/Domain/Entity/HomeService/TechnicalServices.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entity.Home;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entity.HomeService
+{
+    public  class TechnicalServices : HomeBase
+    {
+    }
+}
