f77457a8fa016a7ec36a42d0f3edd1cee2dbcf28
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2024-09-09 22:16:36 +0500

    Add entity Class and add dto folders

---
+++ HomeUI/Domain/Entity/HomeSpareParts/HomeAppliance.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entity.Home;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Domain.Entity.HomeSpareParts
+{
+    public class HomeAppliance : HomeBase
+    {
+    }
+}
