d47d461a3fa7760c36e8c084834b4e9183bdf046
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2024-09-18 00:06:10 +0500

    add code homeBuild

--- HomeUI/Domain/Entity/HomeService/HomeBuild.cs
+++ HomeUI/Domain/Entity/HomeService/HomeBuild.cs
@@ -1,6 +1,7 @@
 ﻿using Domain.Entity.Home;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +10,8 @@ namespace Domain.Entity.HomeService
 {
     public  class HomeBuild :HomeBase
     {
+        [Key]
+        public int Id { get; set; }
+        public string FormatType { get; set; }
     }
 }
