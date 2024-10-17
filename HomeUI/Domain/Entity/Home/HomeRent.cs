8e69b15fa8835c8695ff35e66d023316441bc7c5
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2024-10-18 02:03:22 +0500

    git

--- HomeUI/Domain/Entity/Home/HomeRent.cs
+++ HomeUI/Domain/Entity/Home/HomeRent.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,9 @@ namespace Domain.Entity.Home
 {
     public  class HomeRent : HomeBase
     {
-
+// git checkout master
+//git pull
+//git checkout your-branch
+//git merge master
     }
 }
