c0611c854ada9ff0b9b4e07184a178842f63ce42
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2024-10-18 00:42:44 +0500

    add repair

--- HomeUI/Domain/Entity/HomeService/HomeRepair.cs
+++ HomeUI/Domain/Entity/HomeService/HomeRepair.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,10 @@ namespace Domain.Entity.HomeService
     public  class  HomeRepair : HomeBase
     {
         [Key]
+        [Column("id")]
+        [Required]
         public int Id { get; set; }
+
         public string SizeType { get; set; }
     }
 }
